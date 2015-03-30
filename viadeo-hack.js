621f13d7c82673a4881d190794dbc3fdb7e4f89d
Bruniaux <f.bruniaux@gmail.com>
2015-03-30 21:00:21 +0200

    Update viadeo-hack.js

--- viadeo-hack.js
+++ viadeo-hack.js
@@ -1,23 +1,14 @@
 jQuery('.img.avatar').each(
 	function(){
 		var link = jQuery(jQuery(this).children()[0]); 
-		
 		var img = jQuery(link).children()[0];
-		
 		if(img != undefined){
 			var src = img.src;
-			//console.log(src);
 			if (/memberId/i.test(src)){
 				var regx = /^http:\/\/.*?memberId=(.*)&height=.*/;
-				
 				var id = src.match(regx)[1];		
-				
 				console.log('www.viadeo.com/fr/profile/'+id);	
 			}
 		}
-		
-		
-
-		
 	}
 );
