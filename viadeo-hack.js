89559c850d2b5a4ca9d99d2313859297b0c300ec
BRUNIAUX Florian <web1@e-motors.fr>
2015-03-30 20:47:22 +0200

    Scripts js

---
+++ viadeo-hack.js
@@ -0,0 +1,23 @@
+jQuery('.img.avatar').each(
+	function(){
+		var link = jQuery(jQuery(this).children()[0]); 
+		
+		var img = jQuery(link).children()[0];
+		
+		if(img != undefined){
+			var src = img.src;
+			//console.log(src);
+			if (/memberId/i.test(src)){
+				var regx = /^http:\/\/.*?memberId=(.*)&height=.*/;
+				
+				var id = src.match(regx)[1];		
+				
+				console.log('www.viadeo.com/fr/profile/'+id);	
+			}
+		}
+		
+		
+
+		
+	}
+);
