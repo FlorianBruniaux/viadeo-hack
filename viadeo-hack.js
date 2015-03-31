b98b120871b45172533e0d590159e77c97bd51d1
Bruniaux <f.bruniaux@gmail.com>
2015-03-31 08:34:59 +0200

    Update viadeo-hack.js

--- viadeo-hack.js
+++ viadeo-hack.js
@@ -1,3 +1,4 @@
+//	Le title contenant le nombre de visites
 var h1 = jQuery('.top-h').html();
 var regxNbVisites = /([0-9]+)/;
 
@@ -21,9 +22,11 @@ function parseUrl(){
 				var src = img.src;
 				if (/memberId/i.test(src)){
 					var regxIdMembre = /^http:\/\/.*?memberId=(.*)&height=.*/;
+					//	Id du membre
 					var id = src.match(regxIdMembre)[1];	
 					//	On log le lien de la fiche du visiteur
 					console.log('www.viadeo.com/fr/profile/'+id);	
+					//	On incrémente le nombre de profils trouvés
 					totalParsed++;
 				}
 			}
@@ -33,6 +36,7 @@ function parseUrl(){
 	console.log(totalParsed + " profils identifiés !");
 }
 
+//	Numéro de la page parsée
 var pageOk = 0;
 
 //	On va parcourir toutes les pages
@@ -46,10 +50,16 @@ for(var page = 2; page <= nbPages; page++){
 	}).done(function(){
 		//	On ajoute les visiteurs à la suite des autres
 	  	jQuery('ul.visitors-list').append(lis);
+	  	
+	  	//	On incrémente
 		pageOk++;
+		
+		//	On logue pour savoir où on en est (car ça peut prendre 2/3 minutes)
 		console.log("page num "+pageOk+" : OK");
 		
+		//	Si on a tout récupéré
 		if(pageOk == nbPages-1){
+			//	On recherche les liens vers les profils
 			parseUrl();
 		}
 		
